e563a961cb1c1cc9d135b92785395d0d42f003de
Vladimir Frolov <frolov_vo@inbox.ru>
2023-02-22 21:59:19 +0300

    feat: add users routes

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const users = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', users);
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,6 @@ module.exports.getUserById = (req, res) => {
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: err.message }));
