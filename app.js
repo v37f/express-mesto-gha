4c379047bcdc911b4562f805d7da5f8ae743e998
Vladimir Frolov <frolov_vo@inbox.ru>
2023-02-23 00:08:21 +0300

    refactor: users routes

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const users = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', users);
+app.use((req, res, next) => {
+  req.user = {
+    _id: '63f66500e075bef1d31ccfd2',
+  };
+  next();
+});
+app.use('/users', require('./routes/users'));
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const { getUsers, getUserById, createUser } = require('../controllers/users');
 
-router.get('/users', getUsers);
-router.get('/users/:id', getUserById);
-router.post('/users', createUser);
+router.get('/', getUsers);
+router.post('/', createUser);
+router.get('/:id', getUserById);
 
 module.exports = router;
