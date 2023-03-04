01a95c26f82f03402a4fa35e863d1c3e46b54303
Vladimir Frolov <frolov_vo@inbox.ru>
2023-03-04 22:03:45 +0300

    refactor: app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { NOT_FOUND_STATUS_CODE } = require('./utils/constants');
 
 const { PORT = 3000 } = process.env;
 
@@ -17,7 +18,7 @@ app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Страница не найдена' });
+  res.status(NOT_FOUND_STATUS_CODE).send({ message: 'Страница не найдена' });
 });
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
